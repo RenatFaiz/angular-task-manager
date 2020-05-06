578cae557345bde6942676b8114cd7ca33f2e3ec
RenatFaiz <renatf87@gmail.com>
2020-05-06 17:29:47 +0400

    Added categories names in a left sidebar

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from '../../service/data-handler.service';
+import {Category} from '../../model/Category';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[];
 
-  ngOnInit(): void {
+  constructor(private dataHandlerService: DataHandlerService) {
   }
 
+  // Метод вызывается после инициализации компонента
+  ngOnInit(): void {
+    this.categories = this.dataHandlerService.getCategories();
+    console.log(this.categories);
+  }
 }
+
